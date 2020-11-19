b2bfc38e8ca9e4f61083bd48683334a1ac9b6737
bogdan-zaharia <54269120+bogdan-zaharia@users.noreply.github.com>
2020-11-19 12:37:28 +0200

    Initial commit

---
+++ BaseApi/V1/Factories/EntityFactory.cs
@@ -0,0 +1,20 @@
+using BaseApi.V1.Domain;
+using BaseApi.V1.Infrastructure;
+
+namespace BaseApi.V1.Factories
+{
+    public static class EntityFactory
+    {
+        public static Entity ToDomain(this DatabaseEntity databaseEntity)
+        {
+            //TODO: Map the rest of the fields in the domain object.
+            // More information on this can be found here https://github.com/LBHackney-IT/lbh-base-api/wiki/Factory-object-mappings
+
+            return new Entity
+            {
+                Id = databaseEntity.Id,
+                CreatedAt = databaseEntity.CreatedAt,
+            };
+        }
+    }
+}

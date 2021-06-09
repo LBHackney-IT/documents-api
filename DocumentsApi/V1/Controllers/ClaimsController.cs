7d437da8e306f0ee2da43d29559fc79eb4755531
Neil Mendum <nmendum@gmail.com>
2021-06-09 11:54:21 +0100

    DES-308: temporarily expose document download in ClaimsController
    We're waiting for service token, get/documents/token, to be created

--- DocumentsApi/V1/Controllers/ClaimsController.cs
+++ DocumentsApi/V1/Controllers/ClaimsController.cs
@@ -1,5 +1,8 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Amazon.S3;
 using DocumentsApi.V1.Boundary.Request;
 using DocumentsApi.V1.Boundary.Response.Exceptions;
 using DocumentsApi.V1.UseCase.Interfaces;
@@ -16,16 +19,19 @@ namespace DocumentsApi.V1.Controllers
         private ICreateClaimUseCase _createClaimUseCase;
         private readonly IFindClaimByIdUseCase _findClaimByIdUseCase;
         private readonly IUpdateClaimStateUseCase _updateClaimStateUseCase;
+        private readonly IDownloadDocumentUseCase _downloadDocumentUseCase;
 
         public ClaimsController(
             ICreateClaimUseCase createClaimUseCase,
             IFindClaimByIdUseCase findClaimByIdUseCase,
-            IUpdateClaimStateUseCase updateClaimStateUseCase
+            IUpdateClaimStateUseCase updateClaimStateUseCase,
+            IDownloadDocumentUseCase downloadDocumentUseCase
         )
         {
             _createClaimUseCase = createClaimUseCase;
             _findClaimByIdUseCase = findClaimByIdUseCase;
             _updateClaimStateUseCase = updateClaimStateUseCase;
+            _downloadDocumentUseCase = downloadDocumentUseCase;
         }
 
         /// <summary>
@@ -92,6 +98,38 @@ namespace DocumentsApi.V1.Controllers
             catch (NotFoundException ex)
             {
                 return NotFound(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the document
+        /// </summary>
+        /// <response code="200">Found</response>
+        /// <response code="400">Request contains invalid parameters</response>
+        /// <response code="401">Request lacks valid API token</response>
+        /// <response code="500">Amazon S3 exception</response>
+        [HttpGet]
+        [Route("{documentId}/download")]
+        public async Task<IActionResult> GetDocument([FromRoute] Guid documentId)
+        {
+            try
+            {
+                var result = _downloadDocumentUseCase.Execute(documentId);
+                var documentStream = await result.Item2.ConfigureAwait(true);
+                Response.Headers.Add("Content-Disposition", new ContentDisposition
+                {
+                    FileName = "Document" + result.Item1.FileType,
+                    Inline = false
+                }.ToString());
+                return File(documentStream, result.Item1.FileType);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (AmazonS3Exception e)
+            {
+                return StatusCode(500, e.Message);
             }
         }
     }

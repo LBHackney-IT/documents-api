a056b42332d6e1e3775cd221d372abd41261ce25
Neil Mendum <nmendum@gmail.com>
2021-06-22 08:41:48 +0100

    DES-309: formatting

--- DocumentsApi/V1/Boundary/Response/Exceptions/DocumentUploadException.cs
+++ DocumentsApi/V1/Boundary/Response/Exceptions/DocumentUploadException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace DocumentsApi.V1.Boundary.Response.Exceptions
 {
-    public class DocumentUploadException: Exception
+    public class DocumentUploadException : Exception
     {
         public DocumentUploadException(string message) : base(message) { }
     }

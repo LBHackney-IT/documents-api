c3ecb48aa30278d71b8dec14cfd5067fe452e5da
Bogdan <bogdan@madetech.com>
2022-03-02 16:13:53 +0000

    Update config to make the index.js file available on the lambda

--- DocumentsApi/V1/Gateways/S3Gateway.cs
+++ DocumentsApi/V1/Gateways/S3Gateway.cs
@@ -33,51 +33,7 @@ namespace DocumentsApi.V1.Gateways
                Can be removed when presigned post policies are available in .NET
              */
             //var policyString = await _nodeServices.InvokeAsync<string>("V1/Node/index.js", _options.DocumentsBucketName, "pre-scan/" + document.Id.ToString(), UrlExpirySeconds).ConfigureAwait(true);
-            string javascriptModule =
-                @"const { createPresignedPost } = require('@aws-sdk/s3-presigned-post');
-                const { S3Client } = require('@aws-sdk/client-s3');
-
-                /* TODO: Node being added to create signed Post Policies
-                (see this issue: https://github.com/LBHackney-IT/documents-api/pull/6)
-                Can be removed when presigned post policies are available in .NET
-                */
-                module.exports = (callback, bucketName, key, expiry) => {
-                    try {
-                        const expiryInSeconds = parseInt(expiry);
-                        const client = new S3Client({
-                            region: 'eu-west-2',
-                            endpoint: process.env.S3_API_ENDPOINT,
-                        });
-                        const data = createPresignedPost(client, {
-                            Bucket: bucketName,
-                            Key: key,
-                            Expires: expiryInSeconds,
-                            Fields: {
-                                acl: 'private',
-                                key,
-                                'X-Amz-Server-Side-Encryption': 'AES256',
-                            },
-                            Conditions: [
-                                { bucket: bucketName },
-                                { acl: 'private' },
-                                { key },
-                                { 'x-amz-server-side-encryption': 'AES256' },
-                                ['content-length-range', 1, 500000000], // 500mb
-                                ['starts-with', '$Content-Type', ''],
-                            ],
-                        });
-
-                        return callback(null, JSON.stringify(data));
-                    } catch (err) {
-                        console.log('Failed generating pre-signed upload url', {
-                            error: err,
-                        });
-
-                        return callback(err);
-                    }
-                };
-            ";
-            var policyString = await _nodeJSService.InvokeFromStringAsync<string>(javascriptModule, args: new[] { _options.DocumentsBucketName, "pre-scan/" + "test-name", UrlExpirySeconds }).ConfigureAwait(true);
+            var policyString = await _nodeJSService.InvokeFromFileAsync<string>("V1/Node/index.js", args: new[] { _options.DocumentsBucketName, "pre-scan/" + "test-name", UrlExpirySeconds }).ConfigureAwait(true);
             Console.WriteLine($"The S3 link is: {policyString}");
             return JsonConvert.DeserializeObject<S3UploadPolicy>(policyString);
         }

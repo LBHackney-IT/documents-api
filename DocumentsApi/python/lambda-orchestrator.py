eeb2598632d9c1d675705677716413e69eb9b3f8
Bella <arabella.cockrell@gmail.com>
2022-03-04 13:46:34 +0000

    Amend document orchestrator to check for MIME types

--- DocumentsApi/python/lambda-orchestrator.py
+++ DocumentsApi/python/lambda-orchestrator.py
@@ -1,3 +1,4 @@
+from distutils.file_util import move_file
 import json
 import boto3
 import botocore
@@ -21,7 +22,43 @@ def lambda_handler(event, context):
    # Copy Source Object
    copy_source_object = {'Bucket': bucket_name, 'Key': file_key_name}
 
-   # If this is successful we can continue and copy the file
+   #Whitelist of accepted MIME types
+   accepted_mime_types = [
+    'application/msword', #.doc
+    'application/pdf', #.pdf
+    'application/vnd.apple.numbers', #.numbers
+    'application/vnd.apple.pages', #.pages
+    'application/vnd.ms-excel', #.xls
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', #.xlsx
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', #.docx
+    'image/bmp', #.bmp
+    'image/gif', #.gif
+    'image/heic', #.heic
+    'image/jpeg', #.jpeg or .jpg
+    'image/png', #.png
+    'text/plain', #.txt
+    #'video/3gpp', #.3gpp or .3gp
+    #'video/mp4', #.mp4
+    #'video/quicktime', #.mov or .qt
+   ] 
+
+   #Check for MIME type of file
+   head_object = s3_client.head_object(
+       Bucket=bucket_name,
+       Key=file_key_name,
+   )
+    
+   if head_object['ContentType'] not in accepted_mime_types:
+      print(f"File type {head_object['ContentType']} is not accepted!")
+      print("Deleting the original file")
+      print("s3_client.delete_object", bucket_name, file_key_name)
+      s3_client.delete_object(Bucket=bucket_name, Key=file_key_name)
+      return {
+        'statusCode': 200,
+        'body': json.dumps('Document Orchestrator finished successfully')
+      }
+
+   # If MIME type is accepted, we can continue and copy the file
    try:
        # Get filename only and prepend with tmp as this is the only ephemeral storage lambdas have
        download_path = '/tmp/' + file_key_name.split('/')[-1]
@@ -31,37 +68,44 @@ def lambda_handler(event, context):
    except Exception as e:
         print('An exception occurred: {}'.format(e))
         print("There was an error when attempting to download the file, moving to quarantine")
+        move_file_to_quarantine(file_key_name, copy_source_object, bucket_name, s3_client)
 
-        quarantine_file_key_name = file_key_name.replace('pre-scan/', 'quarantine/')
-        print("s3_client.copy_object", copy_source_object, bucket_name, quarantine_file_key_name)
-        s3_client.copy_object(CopySource=copy_source_object, Bucket=bucket_name, Key=quarantine_file_key_name)
+   move_file_to_clean_and_update_database_document_entity(event, lambda_client, file_key_name, copy_source_object, bucket_name, s3_client)
+   
 
-        print("Deleting the original file")
-        print("s3_client.delete_object", bucket_name, file_key_name)
-        s3_client.delete_object(Bucket=bucket_name, Key=file_key_name)
-        return {
-           'statusCode': 200,
-           'body': json.dumps('Document Orchestrator finished successfully')
-        }
+def move_file_to_quarantine(file_key_name, copy_source_object, bucket_name, s3_client):
+    quarantine_file_key_name = file_key_name.replace('pre-scan/', 'quarantine/')
+    print("s3_client.copy_object", copy_source_object, bucket_name, quarantine_file_key_name)
+    s3_client.copy_object(CopySource=copy_source_object, Bucket=bucket_name, Key=quarantine_file_key_name)
 
-   clean_file_key_name  = file_key_name.replace('pre-scan/', 'clean/')
-   print("s3_client.copy_object", copy_source_object, bucket_name, clean_file_key_name)
-   s3_client.copy_object(CopySource=copy_source_object, Bucket=bucket_name, Key=clean_file_key_name)
+    print("Deleting the original file")
+    print("s3_client.delete_object", bucket_name, file_key_name)
+    s3_client.delete_object(Bucket=bucket_name, Key=file_key_name)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Document Orchestrator finished successfully')
+    }
 
-   print("Deleting the original file")
-   print("s3_client.delete_object", bucket_name, file_key_name)
-   s3_client.delete_object(Bucket=bucket_name, Key=file_key_name)
+def move_file_to_clean_and_update_database_document_entity(event, lambda_client, file_key_name, copy_source_object, bucket_name, s3_client):
+    clean_file_key_name  = file_key_name.replace('pre-scan/', 'clean/')
+    print("s3_client.copy_object", copy_source_object, bucket_name, clean_file_key_name)
+    s3_client.copy_object(CopySource=copy_source_object, Bucket=bucket_name, Key=clean_file_key_name)
 
-   print("Invoking documents-api-malware-scan-successful", clean_file_key_name)
-   # Update the event object with the updated key now we've moved the file to the clean directory. Send this payload to the next lambda
-   event['Records'][0]['s3']['object']['key'] = clean_file_key_name
-   response = lambda_client.invoke(
-       FunctionName = 'documents-api-malware-scan-successful',
-       Payload = json.dumps(event)
-   )
-   print("Response of lambda_client.invoke", response)
+    print("Deleting the original file")
+    print("s3_client.delete_object", bucket_name, file_key_name)
+    s3_client.delete_object(Bucket=bucket_name, Key=file_key_name)
 
-   return {
-       'statusCode': 200,
-       'body': json.dumps('Document Orchestrator finished successfully')
-   }
+    print("Invoking documents-api-malware-scan-successful", clean_file_key_name)
+    # Update the event object with the updated key now we've moved the file to the clean directory. 
+    # Send this payload to the next lambda, which calls the UpdateUploadedDocumentUseCase that updates database with Size, UploadedAt and FileType
+    event['Records'][0]['s3']['object']['key'] = clean_file_key_name
+    response = lambda_client.invoke(
+        FunctionName = 'documents-api-malware-scan-successful',
+        Payload = json.dumps(event)
+    )
+    print("Response of lambda_client.invoke", response)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Document Orchestrator finished successfully')
+    }

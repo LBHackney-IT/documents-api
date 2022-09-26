fa13462c3f883beb748be6547a22ebcba56067ee
asha burathoki <aburathoki@gmail.com>
2022-09-26 16:35:48 +0100

    Add migration file to add description to document.

---
+++ DocumentsApi/Migrations/20220926152941_AddDescriptionToDocument.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocumentsApi.Migrations
+{
+    public partial class AddDescriptionToDocument : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "documents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "documents");
+        }
+    }
+}

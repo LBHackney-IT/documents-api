91e20aff03b50f0ac35809fdc2a59eca8ecc2424
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2022-09-27 09:45:45 +0100

    base64helpers tests

--- DocumentsApi.Tests/V1/Helpers/Base64UrlHelpersTests.cs
+++ DocumentsApi.Tests/V1/Helpers/Base64UrlHelpersTests.cs
@@ -1,6 +1,7 @@
 using System;
 using FluentAssertions;
 using NUnit.Framework;
+using DocumentsApi.V1.Boundary.Response.Exceptions;
 using DocumentsApi.V1.Helpers;
 
 namespace DocumentsApi.Tests.V1.Helpers
@@ -15,5 +16,29 @@ namespace DocumentsApi.Tests.V1.Helpers
             var result = Base64UrlHelpers.StringToByteArray(s);
             result.Should().BeOfType<byte[]>();
         }
+
+        [Test]
+        public void EncodeReturnsExpectedBase64URL()
+        {
+            var s = "Test";
+            var result = Base64UrlHelpers.EncodeToBase64Url(s);
+            result.Should().BeEquivalentTo("VGVzdA");
+        }
+
+        [Test]
+        public void DecodeReturnsExpectedString()
+        {
+            var s = "VGVzdA";
+            var result = Base64UrlHelpers.DecodeFromBase64Url(s);
+            result.Should().BeEquivalentTo("Test");
+        }
+        [Test]
+        public void DecodeThrowsExceptionToMalformedString()
+        {
+            var s = "VGVzdApppppp?";
+            Action act = () => Base64UrlHelpers.DecodeFromBase64Url(s);
+            act.Should().Throw<System.Exception>().WithMessage($"Illegal base64url string!");
+        }
+
     }
 }

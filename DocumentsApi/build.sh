85ab84774ac5c29066fd0c51c0cbca5ac23ad6c6
Charli Wild <charli.wild@charlis-mbp.lan>
2022-11-25 10:31:30 +0000

    initial upgrade of containers to dotnet 6. no package upgrades yet

--- DocumentsApi/build.sh
+++ DocumentsApi/build.sh
@@ -18,6 +18,6 @@ then
 fi
 
 dotnet restore
-dotnet lambda package --configuration release --framework netcoreapp3.1 --output-package ./bin/release/netcoreapp3.1/documents-api.zip
+dotnet lambda package --configuration release --framework net6.0 --output-package ./bin/release/net6.0/documents-api.zip
 
 cd python && zip -rv ../lambda-orchestrator.zip ./*

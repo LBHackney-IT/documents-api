f0900e624cd9346600b310b49cf633676440a648
Miles Alford <miles.alford@hackney.gov.uk>
2025-12-03 22:48:35 +0000

    Add node executable to lambda layer

--- DocumentsApi/build.sh
+++ DocumentsApi/build.sh
@@ -20,4 +20,22 @@ fi
 dotnet restore
 dotnet lambda package --configuration release --framework net8.0 --output-package ./bin/release/net8.0/documents-api.zip
 
+# --- Setup Node Layer ---
+cd DocumentsApi/V1/Node/nodejs
+
+# Create bin directory
+mkdir -p bin
+
+# Download Node v22 (Linux x64)
+curl -O https://nodejs.org/dist/v22.13.0/node-v22.13.0-linux-x64.tar.xz
+
+# Extract contents
+tar -xf node-v22.13.0-linux-x64.tar.xz
+
+# Move binary to bin folder
+mv node-v22.13.0-linux-x64/bin/node bin/
+
+# Cleanup
+rm -rf node-v22.13.0-linux-x64*
+
 cd python && zip -rv ../lambda-orchestrator.zip ./*

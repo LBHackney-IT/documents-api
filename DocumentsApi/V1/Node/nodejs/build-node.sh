48d5e14ad47393b8a5de5e0fcba26b2222e2bcb9
Miles Alford <miles.alford@hackney.gov.uk>
2025-12-04 13:44:50 +0000

    Unzip node xz

--- DocumentsApi/V1/Node/nodejs/build-node.sh
+++ DocumentsApi/V1/Node/nodejs/build-node.sh
@@ -4,18 +4,17 @@ if [ -f /etc/debian_version ]
 then
   apt -qq update
   apt -qq -y install xz-utils
-  apt-get install bzip2
 fi
 
 # Create bin directory
 mkdir bin
 
-
 # Download Node v22 (Linux x64)
 curl -O https://nodejs.org/dist/v22.21.1/node-v22.21.1-linux-x64.tar.xz
 
 # Extract contents
-tar -xjf node-v22.21.1-linux-x64.tar.xz
+unxz node-v22.21.1-linux-x64.tar.xz
+tar -xf node-v22.21.1-linux-x64.tar
 
 # Move binary to bin folder
 mv node-v22.21.1-linux-x64/bin/node bin/

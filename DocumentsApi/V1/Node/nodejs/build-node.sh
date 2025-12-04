0dfe76306764a9833be0ef2fe9ee1be62be3b21d
Miles Alford <miles.alford@hackney.gov.uk>
2025-12-04 12:14:29 +0000

    Update node version in build script

--- DocumentsApi/V1/Node/nodejs/build-node.sh
+++ DocumentsApi/V1/Node/nodejs/build-node.sh
@@ -4,13 +4,13 @@
 mkdir -p bin
 
 # Download Node v22 (Linux x64)
-curl -O https://nodejs.org/dist/v22.13.0/node-v22.13.0-linux-x64.tar.xz
+curl -O https://nodejs.org/dist/v22.22.1/node-v22.21.1-linux-x64.tar.xz
 
 # Extract contents
-tar -xf node-v22.13.0-linux-x64.tar.xz
+tar -xf node-v22.21.1-linux-x64.tar.xz
 
 # Move binary to bin folder
-mv node-v22.13.0-linux-x64/bin/node bin/
+mv node-v22.22.1-linux-x64/bin/node bin/
 
 # Cleanup
-rm -rf node-v22.13.0-linux-x64*
+rm -rf node-v22.22.1-linux-x64*

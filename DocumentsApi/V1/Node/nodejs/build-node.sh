a8636f19c1afee047dadde85e0254adb260d5365
Miles Alford <miles.alford@hackney.gov.uk>
2025-12-04 13:11:12 +0000

    Install xz-utils for nodejs build script

--- DocumentsApi/V1/Node/nodejs/build-node.sh
+++ DocumentsApi/V1/Node/nodejs/build-node.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install xz-utils
+fi
+
 # Create bin directory
 mkdir bin
 
+
 # Download Node v22 (Linux x64)
-curl -sL https://deb.nodesource.com/setup_22.x | bash -
-apt-get update && apt-get install -y nodejs
+curl -O https://nodejs.org/dist/v22.22.1/node-v22.21.1-linux-x64.tar.xz
 
 # Extract contents
-#tar -xjf node-v22.21.1-linux-x64.tar.xz
+tar -xjf node-v22.21.1-linux-x64.tar.xz
 
 # Move binary to bin folder
-#mv node-v22.22.1-linux-x64/bin/node bin/
+mv node-v22.22.1-linux-x64/bin/node bin/
 
 # Cleanup
-#rm -rf node-v22.22.1-linux-x64*
+rm -rf node-v22.22.1-linux-x64*

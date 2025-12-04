5e6658312936620b8c78a8b92003029a197e9e4e
Miles Alford <miles.alford@hackney.gov.uk>
2025-12-04 12:56:28 +0000

    Testing something

--- DocumentsApi/V1/Node/nodejs/build-node.sh
+++ DocumentsApi/V1/Node/nodejs/build-node.sh
@@ -4,13 +4,14 @@
 mkdir bin
 
 # Download Node v22 (Linux x64)
-curl -O https://nodejs.org/dist/v22.22.1/node-v22.21.1-linux-x64.tar.xz
+curl -sL https://deb.nodesource.com/setup_22.x | bash -
+apt-get update && apt-get install -y nodejs
 
 # Extract contents
-tar -xjf node-v22.21.1-linux-x64.tar.xz
+#tar -xjf node-v22.21.1-linux-x64.tar.xz
 
 # Move binary to bin folder
-mv node-v22.22.1-linux-x64/bin/node bin/
+#mv node-v22.22.1-linux-x64/bin/node bin/
 
 # Cleanup
-rm -rf node-v22.22.1-linux-x64*
+#rm -rf node-v22.22.1-linux-x64*
